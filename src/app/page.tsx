99c15000e269ef319542d1113ae69e20bbe17db5
Tanzeel <tanzeel498@gmail.com>
2024-04-15 19:50:28 +0500

    fix tailwind config and add background images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src="/home.jpg"
+          alt="car factory"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 }
